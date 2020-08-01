6f6aad1622952178387b1ecba99c45f7161fafa3
Animenosekai <maxence.osawa@gmail.com>
2020-08-01 16:18:27 +0200

    [v2.0]

--- README.md
+++ README.md
@@ -19,24 +19,26 @@ Gives the language of the given text.
     result_language: The language code (two letters) of the output language.
 
 ### Installation
-You can install python-google-translate via `PIP` the Python Package Index Manager.
+You can install pyGoogleTranslate via `PIP` the Python Package Index Manager.
 
 ```bash
-pip install python-google-translate
+pip install pyGoogleTranslate
 ```
 
-But you also need to install [PhantomJS](https://phantomjs.org/download.html)
-
-On macOS, you can install it with Homebrew:
-```bash
-brew cask install phantomjs
-```
+But you also need to install a compatible browser:
+Browser | browser() command | Installation | Notes
+------------ | ------------- | -------------- | --------------
+PhantomJS | `pyGoogleTranslate.browser('phantomjs')` | On macOS with Homebrew `brew cask install phantomjs`  | PhantomJS is not maintained anymore but is the most lightweight of all three.  ⚠️It seems to not handle Japanese Characters well when rendering.
+Firefox | `pyGoogleTranslate.browser('firefox')` | Install [Firefox](https://www.mozilla.org/en-US/firefox/new/) (with the name Firefox) and install the driver (on macOS with Homebrew `brew install geckodriver`) | I tested pyGoogleTranslate on Firefox and it seems to be quite long to start but works well with Japanese Characters which seemed to not work with PhantomJS
+Chrome | `pyGoogleTranslate.browser('chrome')` | Install [Google Chrome](https://www.google.com/intl/en-US/chrome/) and install the driver (on macOS with Homebrew `brew cask install chromedriver`) | Same as Firefox.
 
 
 ### Usage
 
 ```python
 import pyGoogleTranslate
+>>> pyGoogleTranslate.browser('<browser you want to use>')
+
 
 >>> pyGoogleTranslate.translate('Hello', 'ja')
 'こんにちは'
@@ -53,16 +55,13 @@ import pyGoogleTranslate
 >>> pyGoogleTranslate.detect_language('Nihao')
 'Chinese'
 
->>> pyGoogleTranslate.detect_language('Kono Python module wo daunrodo itadaki makoto ni arigatogozaimashita')
+>>> pyGoogleTranslate.detect_language('このPythonモジュールをダウンロード頂き誠にありがとうございました。')
 'Japanese'
 ```
 
 ### Dependencies
 
 This module has one python module dependency: [Selenium](https://www.selenium.dev/), used to automate browsers which is downloaded when installing python-google-translate with PIP.
-
-But you also need to install [PhantomJS](https://phantomjs.org/download.html), a headless, lightweight browser which I use to get webpages as quick as possible.
-
 
 
 > Google Translate belongs to Google LLC, fully owned by Alphabet Inc.
--- google_translate.py
+++ google_translate.py
@@ -1,14 +1,74 @@
+"""
+pyGoogleTranslate
+
+--> A Google Translate webpage parser for Python 3
+
+⚠️ Do not forget to set the used browser with browser()
+
+© Anime no Sekai - 2020
+"""
+
+import warnings
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
-driver = webdriver.PhantomJS()
+class BrowserError(Exception):
+    """
+    When the browser isn't available.
+    """
+    def __init__(self, msg=None):
+        self.msg = msg 
+    def __str__(self):
+        exception_msg = f"\n\n⚠️⚠️⚠️\n{self.msg}\n"
+        return exception_msg
 
-'''FIREFOX
-from selenium.webdriver.firefox.options import Options
-options = Options()
-options.headless = True
-driver = webdriver.Firefox(options=options)
-'''
+warnings.filterwarnings('ignore')
+
+driver = None
+
+def browser(browser_name, executable_path="PATH"):
+    """
+    To choose the headless browser used by pyGoogleTranslate.\n
+    <executable_path> sets the executable path for your browser.\n
+    If <executable_path> is not empty, pyGoogleTranslate will consider that the browser driver/executable is in your PATH (for example if you downloaded the driver with Homebrew).
+    Browser options:
+        Firefox
+        Chrome
+        PhantomJS
+    """
+    global driver
+    if browser_name.lower() == 'firefox':
+        from selenium.webdriver.firefox.options import Options
+        options = Options()
+        options.headless = True
+        if executable_path == 'PATH':
+            driver = webdriver.Firefox(options=options)
+        else:
+            driver = webdriver.Firefox(options=options, executable_path=executable_path)
+    elif browser_name.lower() == 'chrome':
+        from selenium.webdriver.chrome.options import Options
+        chrome_options = Options()
+        chrome_options.headless = True
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
+        #chrome_options.add_argument("--no-sandbox")
+        if executable_path == 'PATH':
+            driver = webdriver.Chrome(options=chrome_options)
+        else:
+            driver = webdriver.Chrome(options=chrome_options, executable_path=executable_path)
+    elif browser_name.lower() == 'phantom':
+        if executable_path == 'PATH':
+            driver = webdriver.PhantomJS()
+        else:
+            driver = webdriver.PhantomJS(executable_path=executable_path)
+    elif browser_name.lower() == 'phantomjs':
+        if executable_path == 'PATH':
+            driver = webdriver.PhantomJS()
+        else:
+            driver = webdriver.PhantomJS(executable_path=executable_path)
+    else:
+        raise BrowserError(f'{browser_name} is not supported yet.')
 
 def translate(text, destination_language, source_language="auto"):
     """
@@ -17,24 +77,30 @@ def translate(text, destination_language, source_language="auto"):
     Returns a string with the text translated.\n
     Returns "An error occured while translating: translation not found." if the translation was not found in the webpage. This might come from a mistyped language code.
     """
-    try:
-        driver.get(f"https://translate.google.com/?hl=en#view=home&op=translate&sl={source_language}&tl={destination_language}&text={text}")
-        driver.refresh()
-        result = driver.find_element_by_class_name("tlid-translation")
-        return result.text
-    except NoSuchElementException:
-        return "An error occured while translating: translation not found."
-    except:
-        return "An error occured while translating: unknown error."
+    if driver is None:
+        raise BrowserError("Browser is not set yet.\n Please set it with browser()")
+    else:
+        try:
+            driver.get(f"https://translate.google.com/?hl=en#view=home&op=translate&sl={source_language}&tl={destination_language}&text={str(text)}")
+            driver.refresh()
+            result = driver.find_element_by_class_name("tlid-translation")
+            return str(result.text)
+        except NoSuchElementException:
+            return "An error occured while translating: translation not found."
+        except:
+            return "An error occured while translating: unknown error."
 
 def detect_language(text, result_language='en'):
     """
     Returns the language of the given text.
     """
-    driver.get(f"https://translate.google.com/?hl={result_language}#view=home&op=translate&sl=auto&tl=en&text={text}")
-    driver.refresh()
-    raw_result = driver.find_element_by_class_name("jfk-button-checked").get_attribute('innerHTML')
-    result = raw_result.split(' - ')[0]
-    if result == 'Detect language':
-        result = detect_language(text, result_language=result_language)
-    return result
+    if driver is None:
+        raise BrowserError("Browser is not set yet.\n Please set it with browser()")
+    else:
+        driver.get(f"https://translate.google.com/?hl={result_language}#view=home&op=translate&sl=auto&tl=en&text={str(text)}")
+        driver.refresh()
+        raw_result = driver.find_element_by_class_name("jfk-button-checked").get_attribute('innerHTML')
+        result = raw_result.split(' - ')[0]
+        if result == 'Detect language':
+            result = detect_language(text, result_language=result_language)
+        return str(result)
